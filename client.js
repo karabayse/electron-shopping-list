ffe2152a8c3d5db019ff7750f5c4e266289c555f
Kara Bayse <karabayse@gmail.com>
2018-04-15 17:35:21 -0500

    application complete

--- client.js
+++ client.js
@@ -2,7 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+// Set environment
+process.env.NODE_ENV = 'production';
 
 let mainWindow;
 let addWindow;
@@ -17,12 +20,11 @@ app.on('ready', function() {
     protocol: 'file:',
     slashes: true
   }));
-
   // Quit app when closed
   mainWindow.on('closed', function() {
     app.quit();
   });
-  
+
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
@@ -43,6 +45,18 @@ function createAddWindow() {
     protocol: 'file:',
     slashes: true
   }));
+  // Garbage collection handle
+  addWindow.on('close', function() {
+    addWindow = null;
+  });
+
+  // Catch item:add
+  ipcMain.on('item:add', function(e, item) {
+    console.log(item);
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+  });
+
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
@@ -61,7 +75,10 @@ const mainMenuTemplate = [
         }
       },
       {
-        label: 'Clear Items'
+        label: 'Clear Items',
+        click() {
+          mainWindow.webContents.send('item:clear');
+        }
       },
       {
         label: 'Quit',
@@ -73,3 +90,27 @@ const mainMenuTemplate = [
     ]
   }
 ];
+
+// If Mac, add empty object to menu
+if(process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'prooduction') {
+  mainMenuTemplate.push({
+    label: 'Deeloper Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
